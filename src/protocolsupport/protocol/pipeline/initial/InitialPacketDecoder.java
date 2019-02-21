eb47e7c59ac0f55938c3d09934f607c1bdd67e61
Artuto <artutogamer@gmail.com>
2019-02-21 15:22:09 -0600

    different approach

--- src/protocolsupport/protocol/pipeline/initial/InitialPacketDecoder.java
+++ src/protocolsupport/protocol/pipeline/initial/InitialPacketDecoder.java
@@ -206,7 +206,7 @@ public class InitialPacketDecoder extends SimpleChannelInboundHandler<ByteBuf> {
 		ConnectionImpl connection = prepare(channel, version);
 		IPipeLineBuilder builder = IPipeLineBuilder.BUILDERS.get(connection.getVersion());
 		if(builder == null)
-			return;
+			builder = IPipeLineBuilder.BUILDERS.get(ProtocolVersion.MINECRAFT_1_13_2);
 		builder.buildBungeeClientCodec(channel, connection);
 		if (encapsulatedinfo == null) {
 			builder.buildBungeeClientPipeLine(channel, connection);
